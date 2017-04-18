f997bd3d30f5ab448f7d51db59e3877bed98ee99
Zachary Hillman <zohillman@gmail.com>
2017-04-18 11:53:37 -0400

    First

---
+++ app/src/main/java/nqueue/wa/ma/edu/capstoneapp/MainActivity.java
@@ -0,0 +1,13 @@
+package nqueue.wa.ma.edu.capstoneapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
