f90bf212bcf71e9fcb6ab9252ebcca762000feeb
supertaha <supertaha@gmail.com>
2017-04-26 19:08:20 -0400

    messing with different colors

--- app/src/main/java/edu/ma/wa/nqueue/capstoneapp/MainActivity.java
+++ app/src/main/java/edu/ma/wa/nqueue/capstoneapp/MainActivity.java
@@ -2,8 +2,22 @@ package edu.ma.wa.nqueue.capstoneapp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    public Button Exercises, findFriends, Options;
+
+    public void init(){
+        Exercises = (Button)findViewById(R.id.excersise);
+        findFriends = (Button)findViewById(R.id.friends);
+        Options = (Button)findViewById(R.id.options);
+    }
+
+    public void buttonClicked(View v){
+
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
