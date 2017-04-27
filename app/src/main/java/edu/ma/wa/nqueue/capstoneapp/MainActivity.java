e73c4db522bbde7551f707cb74109d1ed1320cab
supertaha <supertaha@gmail.com>
2017-04-26 20:40:48 -0400

    Trying to fix problems

--- app/src/main/java/edu/ma/wa/nqueue/capstoneapp/MainActivity.java
+++ app/src/main/java/edu/ma/wa/nqueue/capstoneapp/MainActivity.java
@@ -1,9 +1,12 @@
 package edu.ma.wa.nqueue.capstoneapp;
 
+import android.content.Intent;
+import android.graphics.Typeface;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -16,12 +19,29 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void buttonClicked(View v){
+        if((Button) v == Exercises){
 
+        }else if((Button) v == findFriends){
+
+        }else if((Button) v == Options) {
+            
+        }
     }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        init();
+        Typeface title = Typeface.createFromAsset(getAssets(), "PWPerspective.ttf");
+        Typeface buttons = Typeface.createFromAsset(getAssets(), "Cheveuxdange.ttf");
+        TextView titleview = (TextView)findViewById(R.id.textviewtitle);
+        TextView buttonview = (TextView)findViewById(R.id.excersise);
+        TextView button2view = (TextView)findViewById(R.id.friends);
+        TextView button3view = (TextView)findViewById(R.id.options);
+        titleview.setTypeface(title);
+        buttonview.setTypeface(buttons);
+        button2view.setTypeface(buttons);
+        button3view.setTypeface(buttons);
     }
 }
