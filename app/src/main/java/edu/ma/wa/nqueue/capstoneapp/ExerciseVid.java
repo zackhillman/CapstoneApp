e03ea2b4b807ac93a39f850f1e80620c57847ec1
supertaha <supertaha@gmail.com>
2017-05-02 19:19:42 -0400

    put ads on all guis

--- app/src/main/java/edu/ma/wa/nqueue/capstoneapp/ExerciseVid.java
+++ app/src/main/java/edu/ma/wa/nqueue/capstoneapp/ExerciseVid.java
@@ -6,6 +6,10 @@ import android.util.Log;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.google.android.gms.ads.AdListener;
+import com.google.android.gms.ads.AdRequest;
+import com.google.android.gms.ads.AdView;
+import com.google.android.gms.ads.InterstitialAd;
 import com.google.android.youtube.player.YouTubeBaseActivity;
 import com.google.android.youtube.player.YouTubeInitializationResult;
 import com.google.android.youtube.player.YouTubePlayer;
@@ -17,6 +21,8 @@ public class ExerciseVid extends YouTubeBaseActivity implements YouTubePlayer.On
     private static final int RECOVERY_REQUEST = 1;
     private YouTubePlayerView youTubeView;
     private int exerciseNum;
+    InterstitialAd mInterstitialAd;
+    private InterstitialAd interstitial;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,6 +32,31 @@ public class ExerciseVid extends YouTubeBaseActivity implements YouTubePlayer.On
         youTubeView = (YouTubePlayerView) findViewById(R.id.youtube_view);
         youTubeView.initialize(Config.YOUTUBE_API_KEY, this);
         ((TextView)findViewById(R.id.textView)).setText(Exercises.tutList[exerciseNum]);
+
+        AdView mAdView = (AdView) findViewById(R.id.adView);
+        AdRequest adRequest = new AdRequest.Builder().build();
+        mAdView.loadAd(adRequest);
+
+        // Prepare the Interstitial Ad
+        interstitial = new InterstitialAd(ExerciseVid.this);
+// Insert the Ad Unit ID
+        interstitial.setAdUnitId(getString(R.string.admob_interstitial_id));
+
+        interstitial.loadAd(adRequest);
+// Prepare an Interstitial Ad Listener
+        interstitial.setAdListener(new AdListener() {
+            public void onAdLoaded() {
+                // Call displayInterstitial() function
+                displayInterstitial();
+            }
+        });
+
+    }
+    public void displayInterstitial() {
+// If Ads are loaded, show Interstitial else show nothing.
+        if (interstitial.isLoaded()) {
+            interstitial.show();
+        }
     }
 
     @Override
--- app/src/main/java/edu/ma/wa/nqueue/capstoneapp/ListExercise.java
+++ app/src/main/java/edu/ma/wa/nqueue/capstoneapp/ListExercise.java
@@ -6,10 +6,17 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
+import com.google.android.gms.ads.AdListener;
+import com.google.android.gms.ads.AdRequest;
+import com.google.android.gms.ads.AdView;
+import com.google.android.gms.ads.InterstitialAd;
+
 
 public class ListExercise extends AppCompatActivity {
 
     public Button Exe1, Exe2, Exe3;
+    InterstitialAd mInterstitialAd;
+    private InterstitialAd interstitial;
 
     public void init(){
         Exe1 = (Button)findViewById(R.id.exercise1);
@@ -36,6 +43,30 @@ public class ListExercise extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_list_exercise);
+        AdView mAdView = (AdView) findViewById(R.id.adView);
+        AdRequest adRequest = new AdRequest.Builder().build();
+        mAdView.loadAd(adRequest);
+
+// Prepare the Interstitial Ad
+        interstitial = new InterstitialAd(ListExercise.this);
+// Insert the Ad Unit ID
+        interstitial.setAdUnitId(getString(R.string.admob_interstitial_id));
+
+        interstitial.loadAd(adRequest);
+// Prepare an Interstitial Ad Listener
+        interstitial.setAdListener(new AdListener() {
+            public void onAdLoaded() {
+                // Call displayInterstitial() function
+                displayInterstitial();
+            }
+        });
         init();
+    }
+
+    public void displayInterstitial() {
+// If Ads are loaded, show Interstitial else show nothing.
+        if (interstitial.isLoaded()) {
+            interstitial.show();
+        }
     }
 }
