3577becd2dd1500a19278f01191fbfa223a5d650
supertaha <supertaha@gmail.com>
2017-05-02 16:39:11 -0400

    implemented ads

--- app/src/main/java/edu/ma/wa/nqueue/capstoneapp/MainActivity.java
+++ app/src/main/java/edu/ma/wa/nqueue/capstoneapp/MainActivity.java
@@ -8,9 +8,17 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
+import com.google.android.gms.ads.AdListener;
+import com.google.android.gms.ads.AdRequest;
+import com.google.android.gms.ads.AdView;
+import com.google.android.gms.ads.InterstitialAd;
+
+
 public class MainActivity extends AppCompatActivity {
 
     public Button Exercises, findFriends, Options;
+    InterstitialAd mInterstitialAd;
+    private InterstitialAd interstitial;
 
     public void init(){
         Exercises = (Button)findViewById(R.id.exercise);
@@ -33,6 +41,32 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        AdView mAdView = (AdView) findViewById(R.id.adView);
+        AdRequest adRequest = new AdRequest.Builder().build();
+        mAdView.loadAd(adRequest);
+
+// Prepare the Interstitial Ad
+        interstitial = new InterstitialAd(MainActivity.this);
+// Insert the Ad Unit ID
+        interstitial.setAdUnitId(getString(R.string.admob_interstitial_id));
+
+        interstitial.loadAd(adRequest);
+// Prepare an Interstitial Ad Listener
+        interstitial.setAdListener(new AdListener() {
+            public void onAdLoaded() {
+                // Call displayInterstitial() function
+                displayInterstitial();
+            }
+        });
         init();
     }
+
+    public void displayInterstitial() {
+// If Ads are loaded, show Interstitial else show nothing.
+        if (interstitial.isLoaded()) {
+            interstitial.show();
+        }
+    }
+
 }
