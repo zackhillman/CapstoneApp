c07a181ca1fee7a349efbf4994504afba4054e72
supertaha <supertaha@gmail.com>
2017-05-05 22:31:31 -0400

    ad stuff

--- app/src/main/java/edu/ma/wa/nqueue/capstoneapp/MainActivity.java
+++ app/src/main/java/edu/ma/wa/nqueue/capstoneapp/MainActivity.java
@@ -8,8 +8,11 @@ import android.os.Bundle;
 import com.google.android.gms.ads.AdRequest;
 import com.google.android.gms.ads.AdView;
 import com.google.android.gms.ads.InterstitialAd;
+import com.google.android.gms.ads.MobileAds;
 import com.roughike.bottombar.BottomBar;
 import com.roughike.bottombar.OnTabSelectListener;
+import com.startapp.android.publish.adsCommon.StartAppAd;
+import com.startapp.android.publish.adsCommon.StartAppSDK;
 
 import edu.ma.wa.nqueue.capstoneapp.Fragment.About;
 import edu.ma.wa.nqueue.capstoneapp.Fragment.Exercise;
@@ -20,12 +23,14 @@ import edu.ma.wa.nqueue.capstoneapp.Fragment.Profile;
 
 public class MainActivity extends AppCompatActivity {
 
-
+    private StartAppAd startAppAd = new StartAppAd(this);
     private InterstitialAd interstitial;
     private BottomBar bar;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        StartAppSDK.init(this, "Your app Id", true);
+        StartAppAd.showSplash(this, savedInstanceState);
         setContentView(R.layout.activity_main);
         if (savedInstanceState == null) {
             getSupportFragmentManager().beginTransaction().add(R.id.frame, new Profile()).commit();
@@ -53,6 +58,7 @@ public class MainActivity extends AppCompatActivity {
             }
 
         });
+        MobileAds.initialize(getApplicationContext(), "ca-app-pub-1018669573856883/9774887054");
         AdView mAdView = (AdView) findViewById(R.id.adView);
         AdRequest adRequest = new AdRequest.Builder().build();
         mAdView.loadAd(adRequest);
@@ -77,6 +83,19 @@ public class MainActivity extends AppCompatActivity {
         if (interstitial.isLoaded()) {
             interstitial.show();
         }
+    }
+
+    @Override    public void onBackPressed() {
+        startAppAd.onBackPressed();
+        super.onBackPressed();
+    }
+    @Override    public void onResume() {
+        super.onResume();
+        startAppAd.onResume();
+    }
+    @Override    public void onPause() {
+        super.onPause();
+        startAppAd.onPause();
     }
 
 }
