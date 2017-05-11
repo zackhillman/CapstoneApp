5c8a2d903c87329d5cbcbdc8e32be978b6d3317c
Josh vasilevsky <Josh vasilevsky>
2017-05-11 18:35:23 -0400

    Sending Data between fragments

--- app/src/main/java/edu/ma/wa/nqueue/capstoneapp/Exercises.java
+++ app/src/main/java/edu/ma/wa/nqueue/capstoneapp/Exercises.java
@@ -1,23 +1,23 @@
 package edu.ma.wa.nqueue.capstoneapp;
 import java.util.HashMap;
+
+import edu.ma.wa.nqueue.capstoneapp.Fragment.Exercise;
+
 /**
  * Created by drewh on 4/27/2017.
  */
 
 public class Exercises {
 
-    public static String[] tutList = new String[]{"To perform this movement you will need a preacher bench and an E-Z bar. Grab the E-Z curl bar at the close inner handle (either have someone hand you the bar which is preferable or grab the bar from the front bar rest provided by most preacher benches). The palm of your hands should be facing forward and they should be slightly tilted inwards due to the shape of the bar.\n" +
-            "With the upper arms positioned against the preacher bench pad and the chest against it, hold the E-Z Curl Bar at shoulder length. This will be your starting position.\n" +
-            "As you breathe in, slowly lower the bar until your upper arm is extended and the biceps is fully stretched.\n" +
-            "As you exhale, use the biceps to curl the weight up until your biceps is fully contracted and the bar is at shoulder height. Squeeze the biceps hard and hold this position for a second.\n", "Pull your shoulder blades down and together. If you're performing back squats (you have a barbell across the back of your shoulders), focus on pulling the bar into your shoulder blades. It'll help you keep your shoulder blades tight throughout the entire movement.\n" +
-            "Then, pretend you're trying to spread the floor between your feet. Push your feet into the ground and out to the sides. Your feet shouldn't actually move, but you should feel some tension in your hips. That will give you some more stability so your knees don't cave in.\n" +
-            "Squat as low as you can comfortably. The ideal depth is going to be different for everyone. But, by and large, you should squat until the top surface of your thigh is just below your knee. Just remember, squats should never hurt. If they hurt, that's your body telling you to change how you're doing them.\n" +
-            "Keep your knees in line with your toes. While it's perfectly OK if your knees extend a centimeter or two in front of your toes, focusing on keeping them behind your toes is an easy way to make sure you don't end up taking things too far forward, he says. Ideally, your hips should move back just as much as your knees move forward.\n", "Lie down on a flat bench with a dumbbell in each hand resting on top of your thighs. The palms of your hands will be facing each other.\n" +
-            "Then, using your thighs to help raise the dumbbells up, lift the dumbbells one at a time so that you can hold them in front of you at shoulder width.\n" +
-            "Once at shoulder width, rotate your wrists forward so that the palms of your hands are facing away from you. The dumbbells should be just to the sides of your chest, with your upper arm and forearm creating a 90 degree angle. Be sure to maintain full control of the dumbbells at all times. This will be your starting position.\n" +
-            "Then, as you breathe out, use your chest to push the dumbbells up. Lock your arms at the top of the lift and squeeze your chest, hold for a second and then begin coming down slowly. Tip: Ideally, lowering the weight should take about twice as long as raising it.\n"};
+    private static ExerciseStep preach = new ExerciseStep(new String[]{"step 1", "Step 2", "step 3", "Step 4", "step 5", "Step 6", "step 7", "Step 8"});
+    private static ExerciseStep squat = new ExerciseStep(new String[]{"step 1", "Step 2"});
+    private static ExerciseStep dumbbell = new ExerciseStep(new String[]{"step 1", "Step 2"});
+
+
+    public static ExerciseStep[] tutList = new ExerciseStep[]{preach, squat, dumbbell };
     public static String[]  linkList = new String[]{"DoCWeUBA0Gs", "t2b8UdqmlFs", "VmB1G1K7v94"};
 
 
 
 }
+
