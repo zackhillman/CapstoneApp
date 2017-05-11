abd4c78a165482359b3f025b5d65fcbbd191ed08
Andrew <Andrew>
2017-05-10 21:21:26 -0400

    No error calendarView

--- app/src/main/java/edu/ma/wa/nqueue/capstoneapp/Fragment/Logs.java
+++ app/src/main/java/edu/ma/wa/nqueue/capstoneapp/Fragment/Logs.java
@@ -7,6 +7,7 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.CalendarView;
+import android.widget.CalendarView.OnDateChangeListener;
 import android.widget.Toast;
 
 import edu.ma.wa.nqueue.capstoneapp.R;
@@ -21,15 +22,24 @@ public class Logs extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View v = inflater.inflate(R.layout.fragment_logs,container,false);
-
         CalendarView calendar = (CalendarView) v.findViewById(R.id.calendar1);
-        calendar.setOnClickListener(new CalendarView.OnDateChangeListener(){
 
-            @Override
-            public void onSelectedDayChange(CalendarView view, int year, int month, int dayofMonth){
-                Toast.makeText(getBaseContext(),"Selected date "+dayofMonth+"/"+month+year, Toast.LENGTH_LONG.show();
-            }
-        });
+        calendar.setOnDateChangeListener(new OnDateChangeListener() {
+             @Override
+             public void onSelectedDayChange(CalendarView view, int year, int month, int day) {
+                 Toast.makeText(getActivity().getApplicationContext(), day + "/" + month + "/" + year, Toast.LENGTH_LONG).show();
+             }
+         });
+
+
+
+//                 calendar.setOnClickListener(new CalendarView.OnDateChangeListener(){
+//
+//            @Override
+//            public void onSelectedDayChange(CalendarView view, int year, int month, int dayofMonth){
+//                Toast.makeText(getContext(),"Selected date "+dayofMonth+"/"+month+year, Toast.LENGTH_LONG).show();
+//            }
+//        });
 
 
 
