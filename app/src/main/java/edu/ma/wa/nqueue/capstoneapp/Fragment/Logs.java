833ceca2e3ca613ffccbb155705d22ad1105e34e
Andrew <Andrew>
2017-05-11 23:03:58 -0400

    Stuff

--- app/src/main/java/edu/ma/wa/nqueue/capstoneapp/Fragment/Logs.java
+++ app/src/main/java/edu/ma/wa/nqueue/capstoneapp/Fragment/Logs.java
@@ -8,6 +8,7 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.CalendarView;
 import android.widget.CalendarView.OnDateChangeListener;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import edu.ma.wa.nqueue.capstoneapp.R;
@@ -23,6 +24,7 @@ public class Logs extends Fragment {
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View v = inflater.inflate(R.layout.fragment_logs,container,false);
         CalendarView calendar = (CalendarView) v.findViewById(R.id.calendar1);
+        TextView eventText = (TextView)v.findViewById(R.id.EventText);
 
         calendar.setOnDateChangeListener(new OnDateChangeListener() {
              @Override
@@ -30,19 +32,6 @@ public class Logs extends Fragment {
                  Toast.makeText(getActivity().getApplicationContext(), day + "/" + month + "/" + year, Toast.LENGTH_LONG).show();
              }
          });
-
-
-
-//                 calendar.setOnClickListener(new CalendarView.OnDateChangeListener(){
-//
-//            @Override
-//            public void onSelectedDayChange(CalendarView view, int year, int month, int dayofMonth){
-//                Toast.makeText(getContext(),"Selected date "+dayofMonth+"/"+month+year, Toast.LENGTH_LONG).show();
-//            }
-//        });
-
-
-
         return v;
     }
 
